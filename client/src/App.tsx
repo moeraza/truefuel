c25ff0d76de5a558d9c424fc63ab29c380a1cce9
moeraza <mohammadraza.pr@gmail.com>
2026-03-18 04:12:42 +0000

    Fix: scroll to top on route change (mobile cart nav)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
-import { Switch, Route, Router } from "wouter";
+import { Switch, Route, Router, useLocation } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { useEffect } from "react";
 import Layout from "@/components/layout";
 import Home from "@/pages/home";
 import Shop from "@/pages/shop";
@@ -14,9 +15,18 @@ import Checkout from "@/pages/checkout";
 import OrderSuccess from "@/pages/order-success";
 import NotFound from "@/pages/not-found";
 
+function ScrollToTop() {
+  const [location] = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  return null;
+}
+
 function AppRouter() {
   return (
     <Layout>
+      <ScrollToTop />
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/shop" component={Shop} />
